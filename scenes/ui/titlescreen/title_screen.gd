f6c5e1e24004a2ea4b9fa0052207f4b633c45377
GabrielSiras <gabrielsironi06@gmail.com>
2026-05-12 23:54:37 -0300

    FEAT: Iniciado TitleScreen e mudado cena principal.

---
+++ scenes/ui/titlescreen/title_screen.gd
@@ -0,0 +1,29 @@
+extends Control
+
+@onready var music_player: AudioStreamPlayer = $AudioStreamPlayer
+@onready var opções: Panel = $Opções
+@onready var botões: VBoxContainer = $Botões
+
+func _ready() -> void:
+	botões.visible = true
+	opções.visible = false
+
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_começar_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/levels/game-basico.tscn")
+
+func _on_creditos_pressed() -> void:
+	pass
+
+func _on_opções_pressed() -> void:
+	botões.visible = false
+	opções.visible = true
+
+func _on_sairdojogo_pressed() -> void:
+	get_tree().quit()
+
+func _on_voltar_opções_pressed() -> void:
+	_ready()
