a665183e2632f46f123370ce1dba14d7c0908894
MatheusMetzUCS <mcmetz@ucs.br>
2026-06-23 18:49:40 -0300

    playtest 1.1

--- scripts/levels/level_select.gd
+++ scripts/levels/level_select.gd
@@ -10,3 +10,19 @@ func _on_btn_fase_1_pressed() -> void:
 func _on_btn_fase_2_pressed() -> void:
 	GameManager.reset_checkpoint_progression()
 	get_tree().change_scene_to_file("res://scenes/levels/Level_02.tscn")
+
+func _on_btn_fase_3_pressed() -> void:
+	GameManager.reset_checkpoint_progression()
+	get_tree().change_scene_to_file("res://scenes/levels/Level_03.tscn")
+
+func _on_btn_fase_4_pressed() -> void:
+	GameManager.reset_checkpoint_progression()
+	get_tree().change_scene_to_file("res://scenes/levels/Level_04.tscn")
+
+func _on_btn_fase_5_pressed() -> void:
+	GameManager.reset_checkpoint_progression()
+	get_tree().change_scene_to_file("res://scenes/levels/Level_05.tscn")
+
+func _on_btn_fase_6_pressed() -> void:
+	GameManager.reset_checkpoint_progression()
+	get_tree().change_scene_to_file("res://scenes/levels/Level_06.tscn")
